784ec9f89bbb192545c74eb8f24049584ab0eaca
Firstname Lastname <tim@timrich.org>
2022-08-10 23:21:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,5 @@ void main() {
   );
 
 
-  void main() {
-    runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('I AM RICH'),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-          backgroundColor: Colors.blueGrey,
-          body: Center(
-            child: Image(
-              image: AssetImage('images/diamond.png'),
-            ),
-          ),
-        ),
-      ),
-    );
+  
 }
